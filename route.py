2400ee94440d0ddc3f3e8b2437689f9d5b2b75dc
Yathish L <yathish.l@lilly.com>
2023-08-28 10:28:44 +0530

    flask Run

--- route.py
+++ route.py
@@ -6,3 +6,8 @@ app=Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html",name="yathish")
+
+if __name__=="__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
