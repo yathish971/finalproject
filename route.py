a2715416ee3f5946d3dd1080b0dce57db8e4f9b4
Yathish L <yathish.l@lilly.com>
2023-08-28 12:11:21 +0530

    database

--- route.py
+++ route.py
@@ -1,11 +1,19 @@
 from flask import Flask,render_template,request,redirect
-import pymysql
-user = 'root'
-password = 'Yathish123@'
-host = '10.0.1.15'
-port = 3306
-database = 'information'
-app=Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:Yathish123@!@10.0.1.15/information'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(80), nullable=False)
+    age = db.Column(db.String(80), nullable=False)
+
 
 @app.route("/")
 @app.route("/index.html",methods=['GET','POST'])
@@ -15,17 +23,17 @@ def hello_world():
         user_email = request.form.get('user_email', '')
         user_password=request.form.get('user_password', '')
         user_age=request.form.get('user_password', '')
-
+        new_user=User(username=user_name,email=user_email,password=user_password,age=user_age)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect("/")
     return render_template("index.html",name="yathish")
 
 
 if __name__=="__main__":
     
-    try:
-        mydb=pymysql.connect(host="10.0.1.15",user="root",password="Yathish123@",database='information')
-        print(mydb)# GET THE CONNECTION OBJECT (ENGINE) FOR THE DATABASE
-    except Exception as e:
-        print(e)
+    with app.app_context():
+        db.create_all()
    
       
-        app.run(debug=True,host="0.0.0.0",port=8080)
+    app.run(debug=True,host="0.0.0.0",port=8080)
